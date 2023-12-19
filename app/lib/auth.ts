610a0ce3dec289a153a589a8fca97249d938b28f
haru <haruki6180@gmail.com>
2023-12-19 15:22:28 +0900

    setup authentication

---
+++ app/lib/auth.ts
@@ -0,0 +1,14 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { prisma } from "./db";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+  ],
+};

01d4aae1d4e7c0208d4985e0e5be9abf0cd41fc1
haru <haruki6180@gmail.com>
2023-12-22 14:53:29 +0900

    send ok

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -19,26 +19,29 @@ export default function ChatComponent({ data }: iAppProps) {
   return (
     <div className="p-6 flex-grow max-h-screen overflow-y-auto py-32">
       <div className="flex flex-col gap-4">
-        {totalComments.map((message, index) => (
-          <div key={index}>
-            <div className="flex items-center">
-              <Image
-                src={message.User?.image as string}
-                alt="Profile image of user"
-                className="w-12 h-12 object-cover rounded-lg mr-4"
-                width={50}
-                height={50}
-              />
-              <div className="rounded-lg bg-white p-4 shadow-md self-start">
-                {message.message}
+        {totalComments.map((message, index) => {
+          console.log(message);
+          return (
+            <div key={index}>
+              <div className="flex items-center">
+                <Image
+                  src={message.User?.image as string}
+                  alt="Profile image of user"
+                  className="w-12 h-12 object-cover rounded-lg mr-4"
+                  width={50}
+                  height={50}
+                />
+                <div className="rounded-lg bg-white p-4 shadow-md self-start">
+                  {message.message}
+                </div>
               </div>
-            </div>
 
-            <p className="font-light text-sm text-gray-600">
-              {message.User?.name}
-            </p>
-          </div>
-        ))}
+              <p className="font-light text-sm text-gray-600">
+                {message.User?.name}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
